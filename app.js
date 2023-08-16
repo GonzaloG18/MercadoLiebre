5eb648264a6385742b89555bc8b8a117eca5a4fa
GonzaloG18 <fragmentado1811@gmail.com>
2023-08-15 22:48:52 -0300

    parche del package y del app

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
 //Levantar el servidor
-
-app.set('puerto', process.env.PORT || 3001);
 
 app.get('/',(req,res)=>{
     res.sendFile(res.resolve(__dirname, "./views","home.html"))
